32cd7daca280a2c8bf97a269621d7e06eaffea6a
Yury Paraschenko <y_paraschenko@wargaming.net>
2020-10-17 22:29:48 +0300

    added telegram #1

--- telegram/apiRequest.go
+++ telegram/apiRequest.go
@@ -1,24 +1,69 @@
 package telegram
 
 import (
+	"errors"
 	"fmt"
+	"io/ioutil"
 	"log"
+	"net/http"
 	"net/url"
+	"time"
 )
 
 var (
 	ApiUrl = `https://api.telegram.org`
+	RequestTimout = time.Second * 3
 )
 
-func makeGetApiRequest(token, method string, parameters url.Values) ([]byte, error) {
+func MakeApiUrl(token, method string, parameters url.Values) (*url.URL, error) {
 	apiUrl, err := url.Parse(fmt.Sprintf("%s/bot%s/%s", ApiUrl, token, method))
 	if err != nil {
 		log.Println(token, method, err.Error())
 		return nil, err
 	}
 	apiUrl.RawQuery = parameters.Encode()
-	log.Println(API_URL)
-	log.Println(apiUrl)
-	log.Println(apiUrl.String())
-	return nil, nil
+	return apiUrl, nil
+}
+
+func MakeGetApiRequest(token, method string, parameters url.Values) (int, []byte, error) {
+	apiUrl, err := MakeApiUrl(token, method, parameters)
+	if err != nil { return 0, nil, err }
+
+	request, err := http.NewRequest(http.MethodGet, apiUrl.String(), nil)
+	if err != nil {
+		log.Println(apiUrl.String(), err.Error())
+		return 0, nil, err
+	}
+
+	client := http.Client{
+		Timeout: RequestTimout,
+	}
+	response, err := client.Do(request)
+	if err != nil {
+		log.Println(apiUrl.String(), err.Error())
+		return 0, nil, err
+	}
+	defer response.Body.Close()
+
+	data, err := ioutil.ReadAll(response.Body)
+	if err != nil {
+		log.Println(apiUrl.String(), err.Error())
+		return 0, nil, err
+	}
+
+	return response.StatusCode, data, nil
+}
+
+func MakeGetApiRequestRetried(token, method string, parameters url.Values, retries int) (int, []byte, error) {
+	retry := 0
+	for {
+		status, data, err := MakeGetApiRequest(token, method, parameters)
+		if err == nil { return status, data, nil }
+		retry = retry + 1
+		if retry >= retries { break }
+		log.Println(`retry`, retry, token, method, parameters)
+	}
+	msg := fmt.Sprintf("%d retries are fail!", retries)
+	log.Println(msg, token, method, parameters)
+	return 0, nil, errors.New(msg)
 }
--- telegram/me.go
+++ telegram/me.go
@@ -1,1 +1,42 @@
 package telegram
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"log"
+	"net/http"
+	"net/url"
+)
+
+type Me struct {
+	FirstName string `json:"first_name"`
+	Id int `json:"id"`
+	CanJoinGroups bool `json:"can_join_groups"`
+	CanReadAllGroupMessages bool `json:"can_read_all_group_messages"`
+	IsBot bool `json:"is_bot"`
+	SupportsInlineQueries bool `json:"supports_inline_queries"`
+	Username string `json:"username"`
+}
+
+func GetMe(token string) (Me, error) {
+	status, data, err := MakeGetApiRequestRetried(token, `getMe`, url.Values{}, 3)
+	if err != nil { return Me{}, err }
+	var response struct {
+		Ok bool `json:"ok"`
+		Result Me `json:"result"`
+	}
+
+	if err := json.Unmarshal(data, &response); err != nil {
+		log.Println(token, string(data), err.Error())
+		return Me{}, err
+	}
+
+	if !response.Ok || status != http.StatusOK {
+		msg := fmt.Sprintf("HTTP Status: %d; JSON ok: %v", status, response.Ok)
+		log.Println(token, msg)
+		return response.Result, errors.New(msg)
+	}
+
+	return response.Result, nil
+}
