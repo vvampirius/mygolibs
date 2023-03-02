e0099795b75687aac264f1d4ed2ef2e56e302b2f
Yury Paraschenko <vvampirius@gmail.com>
2023-03-02 23:42:39 +0300

    telegram API: added EditMessageInt

--- telegram/editMessage.go
+++ telegram/editMessage.go
@@ -6,3 +6,10 @@ type EditMessageIntInlineKeyboardMarkup struct {
 	InlineMessageId string               `json:"inline_message_id"`
 	ReplyMarkup     InlineKeyboardMarkup `json:"reply_markup"`
 }
+
+type EditMessageInt struct {
+	ChatId    int    `json:"chat_id"`
+	MessageId int    `json:"message_id"`
+	Text      string `json:"text"`
+	ParseMode string `json:"parse_mode"`
+}
